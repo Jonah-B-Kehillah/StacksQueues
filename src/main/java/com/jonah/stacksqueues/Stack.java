840983bee1ad1af0c19ed64d8f828281411d3484
Jonah-B-Kehillah <jbarnett2023@kehillahstudent.org>
2022-09-02 12:35:14 -0700

    Added initial implementation of `Stack` class

--- src/main/java/com/jonah/stacksqueues/Stack.java
+++ src/main/java/com/jonah/stacksqueues/Stack.java
@@ -14,22 +14,41 @@ import java.security.InvalidParameterException;
 public class Stack {
 	
 	private Integer[] values;
-	public int size;
-	private int index;
+	private int size;
+	private int index = 0;
 	
 	public Stack(int _size) {
 		if(_size < 1) {
-			throw new InvalidParameterException("The size of the Stack must be greater than 0");
+			throw new RuntimeException("The size of the Stack must be greater than 0");
 		}
 		this.values = new Integer[_size];
 		this.size = _size;
 	}
 	
 	public void push(int value) {
-		
+		if(index == size-1) {
+			throw new RuntimeException("Stack overflow error");
+		}
+		else {
+			this.values[index] = value;
+			index++;
+		}
 	}
-	public int pop(int index){
-		return 0;
+	
+	public int pop(){
+		int popped_value = this.values[this.index];
+		this.values[this.index] = 0;
+		index--;
+		return popped_value;
 	}
+	
+	public int getSize() {return this.size;}
+	public int getFree() {return this.size -
+								this.index;}
+	public int getHead() {
+		if (index < 1) throw new RuntimeException("There are no values in the stack");
+		return this.values[index];
+	}
+	// more functions here
 	
 }
